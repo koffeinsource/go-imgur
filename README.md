62c6d31166b1747a863c11e04831ad8b88198af9
Jens Breitbart <jbreitbart@gmail.com>
2016-04-16 21:05:25 +0200

    Even more tests.

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 [![GoDoc](https://godoc.org/github.com/koffeinsource/go-imgur?status.svg)](https://godoc.org/github.com/koffeinsource/go-imgur)
 [![Build Status](https://travis-ci.org/koffeinsource/go-imgur.svg?branch=master)](https://travis-ci.org/koffeinsource/go-imgur)
 [![Go Report Card](https://goreportcard.com/badge/github.com/koffeinsource/go-imgur)](https://goreportcard.com/report/github.com/koffeinsource/go-imgur)
+[![Coverage Status](
+https://coveralls.io/repos/github/koffeinsource/go-imgur/badge.svg?branch=master)](https://coveralls.io/github/koffeinsource/go-imgur?branch=master)
 
 Go library to use the imgur.com API. Currently only the anonymous part of the API is supported.
---
+++ image_test.go
@@ -0,0 +1,61 @@
+package imgur
+
+import (
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/koffeinsource/go-klogger"
+)
+
+func TestImageImgurSimulated(t *testing.T) {
+	httpC, server := testHTTPClientJSON("{\"data\":{\"id\":\"ClF8rLe\",\"title\":null,\"description\":null,\"datetime\":1451248840,\"type\":\"image\\/jpeg\",\"animated\":false,\"width\":2448,\"height\":3264,\"size\":1071339,\"views\":176,\"bandwidth\":188555664,\"vote\":null,\"favorite\":false,\"nsfw\":null,\"section\":null,\"account_url\":null,\"account_id\":null,\"in_gallery\":false,\"link\":\"http:\\/\\/i.imgur.com\\/ClF8rLe.jpg\"},\"success\":true,\"status\":200}")
+	defer server.Close()
+
+	client := new(Client)
+	client.HTTPClient = httpC
+	client.Log = new(klogger.CLILogger)
+	client.ImgurClientID = "testing"
+
+	img, status, err := client.GetImageInfo("ClF8rLe")
+
+	if err != nil {
+		t.Errorf("GetRateLimit() failed with error: %v", err)
+		t.FailNow()
+	}
+
+	if img.Animated != false || img.Bandwidth != 188555664 || img.Datetime != 1451248840 || img.Description != "" || img.Height != 3264 || img.Width != 2448 || img.ID != "ClF8rLe" || img.Link != "http://i.imgur.com/ClF8rLe.jpg" || img.Views != 176 {
+		t.Fail()
+	}
+
+	if status != 200 {
+		t.Fail()
+	}
+}
+
+func TestImageImgurReal(t *testing.T) {
+	key := os.Getenv("IMGURCLIENTID")
+	if key == "" {
+		t.Skip("IMGURCLIENTID environment variable not set.")
+	}
+
+	client := new(Client)
+	client.HTTPClient = new(http.Client)
+	client.Log = new(klogger.CLILogger)
+	client.ImgurClientID = key
+
+	img, status, err := client.GetImageInfo("ClF8rLe")
+
+	if err != nil {
+		t.Errorf("GetRateLimit() failed with error: %v", err)
+		t.FailNow()
+	}
+
+	if img.Animated != false || img.Datetime != 1451248840 || img.Description != "" || img.Height != 3264 || img.Width != 2448 || img.ID != "ClF8rLe" || img.Link != "http://i.imgur.com/ClF8rLe.jpg" {
+		t.Fail()
+	}
+
+	if status != 200 {
+		t.Fail()
+	}
+}
