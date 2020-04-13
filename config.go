5ff8eb31784af697160b4e4a51939af05e136d66
Jens Breitbart <jbreitbart@gmail.com>
2020-04-13 12:19:42 +0200

    Updated endpoint for payed API

--- config.go
+++ config.go
@@ -2,5 +2,5 @@ package imgur
 
 const (
 	apiEndpoint        = "https://api.imgur.com/3/"
-	apiEndpointMashape = "https://imgur-apiv3.p.mashape.com/3/"
+	apiEndpointMashape = "https://imgur-apiv3.p.rapidapi.com/3/"
 )
