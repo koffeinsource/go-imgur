1f347c93855381b47382bb7bb9c33021ea3c0233
Michael Hobbs <github@psudo.net>
2020-04-29 10:25:27 -0700

    update API models

--- galleryImage.go
+++ galleryImage.go
@@ -15,72 +15,39 @@ type galleryImageInfoDataWrapper struct {
 
 // GalleryImageInfo contains all gallery image information provided by imgur
 type GalleryImageInfo struct {
-	// The ID for the image
-	ID string `json:"id"`
-	// The title of the image.
-	Title string `json:"title"`
-	// Description of the image.
-	Description string `json:"description"`
-	// Time uploaded, epoch time
-	Datetime int `json:"datetime"`
-	// Image MIME type.
-	MimeType string `json:"type"`
-	// is the image animated
-	Animated bool `json:"animated"`
-	// The width of the image in pixels
-	Width int `json:"width"`
-	// The height of the image in pixels
-	Height int `json:"height"`
-	// The size of the image in bytes
-	Size int `json:"size"`
-	// The number of image views
-	Views int `json:"views"`
-	// Bandwidth consumed by the image in bytes
-	Bandwidth int `json:"bandwidth"`
-	// OPTIONAL, the deletehash, if you're logged in as the image owner
-	Deletehash string `json:"deletehash,omitempty"`
-	// The direct link to the the image. (Note: if fetching an animated GIF that was over 20MB in original size, a .gif thumbnail will be returned)
-	Link string `json:"link"`
-	// OPTIONAL, The .gifv link. Only available if the image is animated and type is 'image/gif'.
-	Gifv string `json:"gifv,omitempty"`
-	// OPTIONAL, The direct link to the .mp4. Only available if the image is animated and type is 'image/gif'.
-	Mp4 string `json:"mp4,omitempty"`
-	// OPTIONAL, The direct link to the .webm. Only available if the image is animated and type is 'image/gif'.
-	Webm string `json:"webm,omitempty"`
-	// OPTIONAL, Whether the image has a looping animation. Only available if the image is animated and type is 'image/gif'.
-	Looping bool `json:"looping,omitempty"`
-	// The current user's vote on the album. null if not signed in or if the user hasn't voted on it.
-	Vote string `json:"vote"`
-	// Indicates if the current user favorited the image. Defaults to false if not signed in.
-	Favorite bool `json:"favorite"`
-	// Indicates if the image has been marked as nsfw or not. Defaults to null if information is not available.
-	Nsfw bool `json:"nsfw"`
-	// Number of comments on the gallery album.
-	CommentCount int `json:"comment_count"`
-	// Up to 10 top level comments, sorted by "best".
-	CommentPreview []Comment `json:"comment_preview"`
-	// Topic of the gallery album.
-	Topic string `json:"topic"`
-	// Topic ID of the gallery album.
-	TopicID int `json:"topic_id"`
-	// If the image has been categorized by our backend then this will contain the section the image belongs in. (funny, cats, adviceanimals, wtf, etc)
-	Section string `json:"section"`
-	// The username of the account that uploaded it, or null.
-	AccountURL string `json:"account_url"`
-	// The account ID of the account that uploaded it, or null.
-	AccountID int `json:"account_id"`
-	// Upvotes for the image
-	Ups int `json:"ups"`
-	// Number of downvotes for the image
-	Downs int `json:"downs"`
-	// Upvotes minus downvotes
-	Points int `json:"points"`
-	// Imgur popularity score
-	Score int `json:"score"`
-	// if it's an album or not
-	IsAlbum bool `json:"is_album"`
-	// Current rate limit
-	Limit *RateLimit
+	ID           string     `json:"id"`                   // The ID for the image
+	Title        string     `json:"title"`                // The title of the image.
+	Description  string     `json:"description"`          // Description of the image.
+	Datetime     int        `json:"datetime"`             // Time uploaded, epoch time
+	MimeType     string     `json:"type"`                 // Image MIME type.
+	Animated     bool       `json:"animated"`             // is the image animated
+	Width        int        `json:"width"`                // The width of the image in pixels
+	Height       int        `json:"height"`               // The height of the image in pixels
+	Size         int        `json:"size"`                 // The size of the image in bytes
+	Views        int        `json:"views"`                // The number of image views
+	Bandwidth    int        `json:"bandwidth"`            // Bandwidth consumed by the image in bytes
+	Deletehash   string     `json:"deletehash,omitempty"` // OPTIONAL, the deletehash, if you're logged in as the image owner
+	Link         string     `json:"link"`                 // The direct link to the the image. (Note: if fetching an animated GIF that was over 20MB in original size, a .gif thumbnail will be returned)
+	Gifv         string     `json:"gifv,omitempty"`       // OPTIONAL, The .gifv link. Only available if the image is animated and type is 'image/gif'.
+	Mp4          string     `json:"mp4,omitempty"`        // OPTIONAL, The direct link to the .mp4. Only available if the image is animated and type is 'image/gif'.
+	Mp4Size      int        `json:"mp4_size,omitempty"`   // OPTIONAL, The Content-Length of the .mp4. Only available if the image is animated and type is 'image/gif'. Note that a zero value (0) is possible if the video has not yet been generated
+	Looping      bool       `json:"looping,omitempty"`    // OPTIONAL, Whether the image has a looping animation. Only available if the image is animated and type is 'image/gif'.
+	Vote         string     `json:"vote"`                 // The current user's vote on the album. null if not signed in or if the user hasn't voted on it.
+	Favorite     bool       `json:"favorite"`             // Indicates if the current user favorited the image. Defaults to false if not signed in.
+	Nsfw         bool       `json:"nsfw"`                 // Indicates if the image has been marked as nsfw or not. Defaults to null if information is not available.
+	CommentCount int        `json:"comment_count"`        // Number of comments on the gallery album.
+	Topic        string     `json:"topic"`                // Topic of the gallery album.
+	TopicID      int        `json:"topic_id"`             // Topic ID of the gallery album.
+	Section      string     `json:"section"`              // If the image has been categorized by our backend then this will contain the section the image belongs in. (funny, cats, adviceanimals, wtf, etc)
+	AccountURL   string     `json:"account_url"`          // The username of the account that uploaded it, or null.
+	AccountID    int        `json:"account_id"`           // The account ID of the account that uploaded it, or null.
+	Ups          int        `json:"ups"`                  // Upvotes for the image
+	Downs        int        `json:"downs"`                // Number of downvotes for the image
+	Points       int        `json:"points"`               // Upvotes minus downvotes
+	Score        int        `json:"score"`                // Imgur popularity score
+	IsAlbum      bool       `json:"is_album"`             // if it's an album or not
+	InMostViral  bool       `json:"in_most_viral"`        // Indicates if the album is in the most viral gallery or not.
+	Limit        *RateLimit // Current rate limit
 }
 
 // GetGalleryImageInfo queries imgur for information on a image
--- image.go
+++ image.go
@@ -15,52 +15,30 @@ type imageInfoDataWrapper struct {
 
 // ImageInfo contains all image information provided by imgur
 type ImageInfo struct {
-	// The ID for the image
-	ID string `json:"id"`
-	// The title of the image.
-	Title string `json:"title"`
-	// Description of the image.
-	Description string `json:"description"`
-	// Time uploaded, epoch time
-	Datetime int `json:"datetime"`
-	// Image MIME type.
-	MimeType string `json:"type"`
-	// is the image animated
-	Animated bool `json:"animated"`
-	// The width of the image in pixels
-	Width int `json:"width"`
-	// The height of the image in pixels
-	Height int `json:"height"`
-	// The size of the image in bytes
-	Size int `json:"size"`
-	// The number of image views
-	Views int `json:"views"`
-	// Bandwidth consumed by the image in bytes
-	Bandwidth int `json:"bandwidth"`
-	// OPTIONAL, the deletehash, if you're logged in as the image owner
-	Deletehash string `json:"deletehash,omitempty"`
-	// OPTIONAL, the original filename, if you're logged in as the image owner
-	Name string `json:"name,omitempty"`
-	// If the image has been categorized by our backend then this will contain the section the image belongs in. (funny, cats, adviceanimals, wtf, etc)
-	Section string `json:"section"`
-	// The direct link to the the image. (Note: if fetching an animated GIF that was over 20MB in original size, a .gif thumbnail will be returned)
-	Link string `json:"link"`
-	// OPTIONAL, The .gifv link. Only available if the image is animated and type is 'image/gif'.
-	Gifv string `json:"gifv,omitempty"`
-	// OPTIONAL, The direct link to the .mp4. Only available if the image is animated and type is 'image/gif'.
-	Mp4 string `json:"mp4,omitempty"`
-	// OPTIONAL, The direct link to the .webm. Only available if the image is animated and type is 'image/gif'.
-	Webm string `json:"webm,omitempty"`
-	// OPTIONAL, Whether the image has a looping animation. Only available if the image is animated and type is 'image/gif'.
-	Looping bool `json:"looping,omitempty"`
-	// Indicates if the current user favorited the image. Defaults to false if not signed in.
-	Favorite bool `json:"favorite"`
-	// Indicates if the image has been marked as nsfw or not. Defaults to null if information is not available.
-	Nsfw bool `json:"nsfw"`
-	// The current user's vote on the album. null if not signed in, if the user hasn't voted on it, or if not submitted to the gallery.
-	Vote string `json:"vote"`
-	// Current rate limit
-	Limit *RateLimit
+	ID          string     `json:"id"`                   // The ID for the image
+	Title       string     `json:"title"`                // The title of the image.
+	Description string     `json:"description"`          // Description of the image.
+	Datetime    int        `json:"datetime"`             // Time uploaded, epoch time
+	MimeType    string     `json:"type"`                 // Image MIME type.
+	Animated    bool       `json:"animated"`             // is the image animated
+	Width       int        `json:"width"`                // The width of the image in pixels
+	Height      int        `json:"height"`               // The height of the image in pixels
+	Size        int        `json:"size"`                 // The size of the image in bytes
+	Views       int        `json:"views"`                // The number of image views
+	Bandwidth   int        `json:"bandwidth"`            // Bandwidth consumed by the image in bytes
+	Deletehash  string     `json:"deletehash,omitempty"` // OPTIONAL, the deletehash, if you're logged in as the image owner
+	Name        string     `json:"name,omitempty"`       // OPTIONAL, the original filename, if you're logged in as the image owner
+	Section     string     `json:"section"`              // If the image has been categorized by our backend then this will contain the section the image belongs in. (funny, cats, adviceanimals, wtf, etc)
+	Link        string     `json:"link"`                 // The direct link to the the image. (Note: if fetching an animated GIF that was over 20MB in original size, a .gif thumbnail will be returned)
+	Gifv        string     `json:"gifv,omitempty"`       // OPTIONAL, The .gifv link. Only available if the image is animated and type is 'image/gif'.
+	Mp4         string     `json:"mp4,omitempty"`        // OPTIONAL, The direct link to the .mp4. Only available if the image is animated and type is 'image/gif'.
+	Mp4Size     int        `json:"mp4_size,omitempty"`   // OPTIONAL, The Content-Length of the .mp4. Only available if the image is animated and type is 'image/gif'. Note that a zero value (0) is possible if the video has not yet been generated
+	Looping     bool       `json:"looping,omitempty"`    // OPTIONAL, Whether the image has a looping animation. Only available if the image is animated and type is 'image/gif'.
+	Favorite    bool       `json:"favorite"`             // Indicates if the current user favorited the image. Defaults to false if not signed in.
+	Nsfw        bool       `json:"nsfw"`                 // Indicates if the image has been marked as nsfw or not. Defaults to null if information is not available.
+	Vote        string     `json:"vote"`                 // The current user's vote on the album. null if not signed in, if the user hasn't voted on it, or if not submitted to the gallery.
+	InGallery   bool       `json:"in_gallery"`           // True if the image has been submitted to the gallery, false if otherwise.
+	Limit       *RateLimit // Current rate limit
 }
 
 // GetImageInfo queries imgur for information on a image
