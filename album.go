b549a10c09383b94bf902d132f86b98ab3100756
Michael Hobbs <github@psudo.net>
2020-04-29 10:25:27 -0700

    update API models

--- album.go
+++ album.go
@@ -15,48 +15,28 @@ type albumInfoDataWrapper struct {
 
 // AlbumInfo contains all album information provided by imgur
 type AlbumInfo struct {
-	// The ID for the album
-	ID string `json:"id"`
-	// The title of the album in the gallery
-	Title string `json:"title"`
-	// The description of the album in the gallery
-	Description string `json:"description"`
-	// Time inserted into the gallery, epoch time
-	DateTime int `json:"datetime"`
-	// The ID of the album cover image
-	Cover string `json:"cover"`
-	// The width, in pixels, of the album cover image
-	CoverWidth int `json:"cover_width"`
-	// The height, in pixels, of the album cover image
-	CoverHeight int `json:"cover_height"`
-	// The account username or null if it's anonymous.
-	AccountURL string `json:"account_url"`
-	// The account ID or null if it's anonymous.
-	AccountID int `json:"account_id"`
-	// The privacy level of the album, you can only view public if not logged in as album owner
-	Privacy string `json:"privacy"`
-	// The view layout of the album.
-	Layout string `json:"layout"`
-	// The number of album views
-	Views int `json:"views"`
-	// The URL link to the album
-	Link string `json:"link"`
-	// Indicates if the current user favorited the image. Defaults to false if not signed in.
-	Favorite bool `json:"favorite"`
-	// Indicates if the image has been marked as nsfw or not. Defaults to null if information is not available.
-	Nsfw bool `json:"nsfw"`
-	// If the image has been categorized by our backend then this will contain the section the image belongs in. (funny, cats, adviceanimals, wtf, etc)
-	Section string `json:"section"`
-	// Order number of the album on the user's album page (defaults to 0 if their albums haven't been reordered)
-	Order int `json:"order"`
-	// OPTIONAL, the deletehash, if you're logged in as the album owner
-	Deletehash string `json:"deletehash,omitempty"`
-	// The total number of images in the album
-	ImagesCount int `json:"images_count"`
-	// An array of all the images in the album (only available when requesting the direct album)
-	Images []ImageInfo `json:"images"`
-	// Current rate limit
-	Limit *RateLimit
+	ID          string      `json:"id"`                   // The ID for the album
+	Title       string      `json:"title"`                // The title of the album in the gallery
+	Description string      `json:"description"`          // The description of the album in the gallery
+	DateTime    int         `json:"datetime"`             // Time inserted into the gallery, epoch time
+	Cover       string      `json:"cover"`                // The ID of the album cover image
+	CoverWidth  int         `json:"cover_width"`          // The width, in pixels, of the album cover image
+	CoverHeight int         `json:"cover_height"`         // The height, in pixels, of the album cover image
+	AccountURL  string      `json:"account_url"`          // The account username or null if it's anonymous.
+	AccountID   int         `json:"account_id"`           // The account ID or null if it's anonymous.
+	Privacy     string      `json:"privacy"`              // The privacy level of the album, you can only view public if not logged in as album owner
+	Layout      string      `json:"layout"`               // The view layout of the album.
+	Views       int         `json:"views"`                // The number of album views
+	Link        string      `json:"link"`                 // The URL link to the album
+	Favorite    bool        `json:"favorite"`             // Indicates if the current user favorited the image. Defaults to false if not signed in.
+	Nsfw        bool        `json:"nsfw"`                 // Indicates if the image has been marked as nsfw or not. Defaults to null if information is not available.
+	Section     string      `json:"section"`              // If the image has been categorized by our backend then this will contain the section the image belongs in. (funny, cats, adviceanimals, wtf, etc)
+	Order       int         `json:"order"`                // Order number of the album on the user's album page (defaults to 0 if their albums haven't been reordered)
+	Deletehash  string      `json:"deletehash,omitempty"` // OPTIONAL, the deletehash, if you're logged in as the album owner
+	ImagesCount int         `json:"images_count"`         // The total number of images in the album
+	Images      []ImageInfo `json:"images"`               // An array of all the images in the album (only available when requesting the direct album)
+	InGallery   bool        `json:"in_gallery"`           // True if the image has been submitted to the gallery, false if otherwise.
+	Limit       *RateLimit  // Current rate limit
 }
 
 // GetAlbumInfo queries imgur for information on a album
--- galleryAlbum.go
+++ galleryAlbum.go
@@ -15,62 +15,34 @@ type galleryAlbumInfoDataWrapper struct {
 
 // GalleryAlbumInfo contains all information provided by imgur of a gallery album
 type GalleryAlbumInfo struct {
-	// The ID for the album
-	ID string `json:"id"`
-	// The title of the album in the gallery
-	Title string `json:"title"`
-	// The description of the album in the gallery
-	Description string `json:"description"`
-	// Time inserted into the gallery, epoch time
-	DateTime int `json:"datetime"`
-	// The ID of the album cover image
-	Cover string `json:"cover"`
-	// The width, in pixels, of the album cover image
-	CoverWidth int `json:"cover_width"`
-	// The height, in pixels, of the album cover image
-	CoverHeight int `json:"cover_height"`
-	// The account username or null if it's anonymous.
-	AccountURL string `json:"account_url"`
-	// The account ID or null if it's anonymous.
-	AccountID int `json:"account_id"`
-	// The privacy level of the album, you can only view public if not logged in as album owner
-	Privacy string `json:"privacy"`
-	// The view layout of the album.
-	Layout string `json:"layout"`
-	// The number of album views
-	Views int `json:"views"`
-	// The URL link to the album
-	Link string `json:"link"`
-	// Upvotes for the image
-	Ups int `json:"ups"`
-	// Number of downvotes for the image
-	Downs int `json:"downs"`
-	// Upvotes minus downvotes
-	Points int `json:"points"`
-	// Imgur popularity score
-	Score int `json:"score"`
-	// if it's an album or not
-	IsAlbum bool `json:"is_album"`
-	// The current user's vote on the album. null if not signed in or if the user hasn't voted on it.
-	Vote string `json:"vote"`
-	// Indicates if the current user favorited the image. Defaults to false if not signed in.
-	Favorite bool `json:"favorite"`
-	// Indicates if the image has been marked as nsfw or not. Defaults to null if information is not available.
-	Nsfw bool `json:"nsfw"`
-	// Number of comments on the gallery album.
-	CommentCount int `json:"comment_count"`
-	// Up to 10 top level comments, sorted by "best".
-	CommentPreview []Comment `json:"comment_preview"`
-	// Topic of the gallery album.
-	Topic string `json:"topic"`
-	// Topic ID of the gallery album.
-	TopicID int `json:"topic_id"`
-	// The total number of images in the album
-	ImagesCount int `json:"images_count"`
-	// An array of all the images in the album (only available when requesting the direct album)
-	Images []ImageInfo `json:"images,omitempty"`
-	// Current rate limit
-	Limit *RateLimit
+	ID           string      `json:"id"`               // The ID for the album
+	Title        string      `json:"title"`            // The title of the album in the gallery
+	Description  string      `json:"description"`      // The description of the album in the gallery
+	DateTime     int         `json:"datetime"`         // Time inserted into the gallery, epoch time
+	Cover        string      `json:"cover"`            // The ID of the album cover image
+	CoverWidth   int         `json:"cover_width"`      // The width, in pixels, of the album cover image
+	CoverHeight  int         `json:"cover_height"`     // The height, in pixels, of the album cover image
+	AccountURL   string      `json:"account_url"`      // The account username or null if it's anonymous.
+	AccountID    int         `json:"account_id"`       // The account ID or null if it's anonymous.
+	Privacy      string      `json:"privacy"`          // The privacy level of the album, you can only view public if not logged in as album owner
+	Layout       string      `json:"layout"`           // The view layout of the album.
+	Views        int         `json:"views"`            // The number of album views
+	Link         string      `json:"link"`             // The URL link to the album
+	Ups          int         `json:"ups"`              // Upvotes for the image
+	Downs        int         `json:"downs"`            // Number of downvotes for the image
+	Points       int         `json:"points"`           // Upvotes minus downvotes
+	Score        int         `json:"score"`            // Imgur popularity score
+	IsAlbum      bool        `json:"is_album"`         // if it's an album or not
+	Vote         string      `json:"vote"`             // The current user's vote on the album. null if not signed in or if the user hasn't voted on it.
+	Favorite     bool        `json:"favorite"`         // Indicates if the current user favorited the image. Defaults to false if not signed in.
+	Nsfw         bool        `json:"nsfw"`             // Indicates if the image has been marked as nsfw or not. Defaults to null if information is not available.
+	CommentCount int         `json:"comment_count"`    // Number of comments on the gallery album.
+	Topic        string      `json:"topic"`            // Topic of the gallery album.
+	TopicID      int         `json:"topic_id"`         // Topic ID of the gallery album.
+	ImagesCount  int         `json:"images_count"`     // The total number of images in the album
+	Images       []ImageInfo `json:"images,omitempty"` // An array of all the images in the album (only available when requesting the direct album)
+	InMostViral  bool        `json:"in_most_viral"`    // Indicates if the album is in the most viral gallery or not.
+	Limit        *RateLimit  // Current rate limit
 }
 
 // GetGalleryAlbumInfo queries imgur for information on a gallery album
